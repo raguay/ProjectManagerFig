2f8407a7b33ff92ecd587e423f0ca7fc2ac3e94e
raguay <raguay@customct.com>
2020-09-21 21:23:31 +0700

    Darkmode with Project Selector.

--- src/components/ProjectSelector.svelte
+++ src/components/ProjectSelector.svelte
@@ -1,31 +1,42 @@
-<div id='projectSelect' style="background-color: {darkmode ? styles.DMbkgrdcolor : styles.bkgrdcolor}; color: {darkmode ? styles.DMcolor : styles.color};">
-  <select
-    bind:value={project}
-    on:change={changeProject}
-    style="background-color: {darkmode ? styles.DMbkgrdcolor : styles.bkgrdcolor}; color: {darkmode ? styles.DMcolor : styles.color};"
-  >
-    {#each projects as proj}
-      <option value={proj[0]}
-              style="background-color: {darkmode ? styles.DMbkgrdcolor : styles.bkgrdcolor}; color: {darkmode ? styles.DMcolor : styles.color};"
-      >{proj[0]}</option>
-    {/each}
-  </select>
+<div id='projectSelect' style="--border: 2px solid {darkmode ? styles.DMcolor : styles.color}; --itemHoverBG: {darkmode ? styles.DMhvbkgrdcolor : styles.hvbkgrdcolor}; --background: {darkmode ? styles.DMbkgrdcolor : styles.bkgrdcolor}; --listBackground: {darkmode ? styles.DMbkgrdcolor : styles.bkgrdcolor}; background-color: {darkmode ? styles.DMbkgrdcolor : styles.bkgrdcolor}; --itemColor: {darkmode ? styles.DMcolor : styles.color}; color: {darkmode ? styles.DMcolor : styles.color};">
+  <Select {items} selectedValue={project} listPlacement={'bottom'} on:select={changeProject}>
+  </Select>
 </div>
 
 <style>
+  #projectSelect {
+    width: 100%;
+    padding: 0px;
+    margin: 0px;
+  }
 </style>
 
 <script>
   import { createEventDispatcher } from 'svelte';
+  import Select from 'svelte-select';
 
-  export let project = '';
-  export let projects = [];
+  export let project;
+  export let projects;
   export let darkmode;
   export let styles;
   
+  let items;
+
   const dispatch = createEventDispatcher();
-  
-  function changeProject() {
-    dispatch('projectChange', project);
+
+  $: items = createList(projects);
+ 
+  function changeProject(proj) {
+    dispatch('projectChange', proj.detail.value);
+  }
+
+  function createList(things) {
+    return things.map(item => { 
+      return {
+        value: item[0], 
+        label: item[0],
+        group: 'projects'
+      }
+    });
   }
 </script>
